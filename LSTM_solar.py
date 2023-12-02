09e9b80f298739d43e4665b005a2324d5c17853f
Murselkoyuncu <93550641+Murselkoyuncu@users.noreply.github.com>
2023-12-02 16:18:47 +0300

    Update LSTM_solar.py

--- LSTM_solar.py
+++ LSTM_solar.py
@@ -4,21 +4,20 @@ Created on Sun Nov 12 20:17:45 2023
 
 @author: mrslk
 """
-#preprocessing
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 # Load the data
 df = pd.read_csv("Solar.csv")
 df.dropna(inplace=True)
 
 # Extract the training and test sets
-training_set = df.iloc[:8712, 1:2].values
-test_set = df.iloc[:8712, 1:2].values
+training_set = df.iloc[:-48, 1:2].values  # Use all data except the last 48 points for training
+test_set = df.iloc[-48:, 1:2].values  # Use the last 48 points for testing
 
 # Feature scaling
 sc = MinMaxScaler(feature_range=(0, 1))
@@ -41,30 +40,79 @@ model_P = Sequential()
 model_P.add(LSTM(units=60, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model_P.add(Dropout(0.2))
 model_P.add(LSTM(units=60, return_sequences=True))
-model_P.add(Dropout(0.2)) 
+model_P.add(Dropout(0.2))
 model_P.add(LSTM(units=60, return_sequences=True))
-model_P.add(Dropout(0.2)) 
+model_P.add(Dropout(0.2))
 model_P.add(LSTM(units=60))
-model_P.add(Dropout(0.2)) 
+model_P.add(Dropout(0.2))
 model_P.add(Dense(units=1))
 
 model_P.compile(optimizer='adam', loss='mean_squared_error')
 
 # Save the model
-model_P.save('LSTM-univeriat')
+model_P.save('LSTM-univariate')
 
 # Load the model
 from keras.models import load_model
-model_P = load_model('LSTM-univeriat')
+model_P = load_model('LSTM-univariate')
 
 # Fit the model
-history = model_P.fit(x_train, y_train, epochs=30, batch_size=32)
+history = model_P.fit(x_train, y_train, epochs=10, batch_size=32)
 
 # Plot the training loss
 plt.plot(range(len(history.history['loss'])), history.history['loss'])
 plt.xlabel('Epoch number')
 plt.ylabel('Loss')
 plt.show()
+
+# Predictions
+prediction_test = []
+batch_one = training_set_scaled[-windowsize:]
+batch_new = batch_one.reshape(1, windowsize, 1)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Assuming you have defined the variables batch_one, windowsize, model_P, test_set, and sc
+
+prediction_test = []
+
+for i in range(720): # one month 
+    batch_one_reshaped = batch_one.reshape(1, windowsize, 1)
+    first_pred = model_P.predict(batch_one_reshaped)[0, 0]
+    prediction_test = np.append(prediction_test, first_pred)
+    
+    # Update batch_new with the new prediction
+    batch_new = np.concatenate((batch_new[:, 1:, :], first_pred.reshape(1, 1, 1)), axis=1)
+    
+    # Update batch_one for the next iteration
+    batch_one = batch_new[:, -windowsize:, :]
+
+# Reshape prediction_test to match the shape of the original data
+prediction_test = prediction_test.reshape(-1, 1)
+
+# Inverse transform the scaled predictions
+prediction = sc.inverse_transform(prediction_test)
+
+plt.plot(test_set, color='red', label='Actual values')
+plt.plot(prediction, color='blue', label='Predicted values')
+plt.title('LSTM Univariate forecast')
+plt.xlabel('time(h)')
+plt.ylabel('solar Irradiance')
+plt.legend()
+plt.show()
+
+# Reshape prediction to match the shape of the original data
+prediction = prediction[:len(test_set)]
+
+# Calculate evaluation metrics
+RMSE = math.sqrt(mean_squared_error(test_set, prediction))
+Rsquare = r2_score(test_set, prediction)
+
+print(f"Root Mean Squared Error (RMSE): {RMSE}")
+print(f"R-squared (R^2): {Rsquare}") 
 
 
 
